afd2d12fd48c50c001f8f2493e95463cd679af02
Chris Yoon <chy.yoon@gmail.com>
2020-08-12 15:46:51 -0700

    added api call
    Co-authored-by: meganhepner <megan.hepner@gmail.com>

---
+++ MvcApiCall/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcapicall.Models;
+
+namespace mvcapicall.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+            return View(allArticles);
+
+        }
+
+    }
+}
